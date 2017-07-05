5fc503b7b51e8dbde625a5c32e884127955734a9
Christoph Ladurner <christoph.ladurner@tugraz.at>
2017-07-05 23:07:19 +0200

    add the client libraries to the repository
    little note, the hbs plugin for requirejs has had to little bugs. the
    first is, it is strange, that the plugin has to load new versions of
    existing loaded libraries of underscore, handlebars. the second is, it
    is not necessary to load the hbs file with mimetype xml, text/plain is
    enough. this change prevents the xml parser error.

--- client/app.js
+++ client/app.js
@@ -2,13 +2,14 @@ require.config({
   scriptType: 'text/javascript;version=1.8',
   baseUrl: '.',
   paths: {
-    jquery: 'lib/jquery/jquery',
-    underscore: 'lib/underscore/underscore',
-    backbone: 'lib/backbone/backbone',
-    handlebars: 'lib/handlebars/dist/handlebars',
-    log: 'lib/loglevel/lib/loglevel',
-    hbs: 'lib/require-handlebars-plugin/hbs',
-    d3: 'lib/d3/d3'
+    jquery: 'lib/jquery',
+    underscore: 'lib/underscore',
+    backbone: 'lib/backbone',
+    handlebars: 'lib/handlebars',
+    log: 'lib/loglevel',
+    d3: 'lib/d3',
+    hbs: 'lib/hbs',
+    json2: 'lib/json2'
   }
 });
 
