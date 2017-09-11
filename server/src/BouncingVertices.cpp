b62bd125b15cdecccb360f2d4b9f6edc36092a8c
Christoph Ladurner <christoph.ladurner@tugraz.at>
2017-09-11 20:41:26 +0200

    BouncingVertices refactored
    moved the calculation of reflex points and convex points to there one
    function. this improves the possibility of a initialization of the
    reflex and convex points on the calculated indices.

--- server/src/BouncingVertices.cpp
+++ server/src/BouncingVertices.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <iterator>
 #include <vector>
+#include <string>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
@@ -16,16 +17,83 @@ using cgal = CGAL::Exact_predicates_inexact_constructions_kernel;
 
 using Segments = std::vector<cgal::Segment_2>;
 
+struct ConvexFilter {
+  size_t N;
+  int point_max = -1;
+  int chain_max = -1;
+  std::vector<bool> vec;
+
+  ConvexFilter(size_t n, int points, int chains)
+    : N(n), point_max(points), chain_max(chains), vec(N, false)
+  {}
+
+  bool isSet(unsigned index) const {
+    return vec[index];
+  }
+
+  void set(unsigned index) {
+    vec[index] = true;
+  }
+
+  explicit operator bool() const {
+    return -1 < point_max || -1 < chain_max;
+  }
+
+  // this function could be used, to do that, what it is done in calculateReflexFilter
+  // void initPoint(unsigned index) {}
+};
+
+struct ReflexFilter {
+  size_t N;
+  int point_max;
+  int chain_max;
+  std::vector<bool> vec;
+  std::vector<int> indices;
+
+  ReflexFilter(size_t n, int points, int chains)
+    : N(n), point_max(points), chain_max(chains), vec(N, false), indices{}
+  {}
+
+  bool isSet(unsigned index) const {
+    return vec[index];
+  }
+
+  void set(unsigned index) {
+    vec[index] = true;
+    indices.push_back(index);
+  }
+
+  explicit operator bool() const {
+    return -1 < point_max || -1 < chain_max;
+  }
+
+  std::vector<int> getIndices() const {
+    return indices;
+  }
+
+  // this function could be used, to do that, what it is done in calculateReflexFilter
+  // void initPoint(unsigned index) {}
+};
+
 
 static Segments::iterator next(Segments &segments, const Segments::iterator &it);
 static Segments::iterator prev(Segments &segments, const Segments::iterator &it);
+
 static std::tuple<pl::SamplingGrid, unsigned, double> init(const pl::CommonSettingList &common_settings);
 static Segments init(const pl::PointList &point_list);
+
+static ConvexFilter calculateConvexFilter(const pl::FilterList &filters, size_t N);
+static ReflexFilter calculateReflexFilter(const pl::FilterList &filters, size_t N);
+
+static void calculateConvexPoints(ConvexFilter &convex_filter, const ReflexFilter &/*reflex_filter*/);
+static void calculateReflexPoints(ReflexFilter &reflex_filter, Segments &segments, const pl::SamplingGrid &sampling_grid, const double radius, const ConvexFilter &convex_filter);
+
 static cgal::Point_2 createPointInsideArea(const pl::SamplingGrid &sampling_grid, const double radius, const Segments::iterator &sit);
+
 static bool isConvex(const cgal::Segment_2 &e_1, const cgal::Segment_2 &e_2, const cgal::Point_2 &shifted_point,
-                     Segments &segments, const std::vector<bool> &reflex_point_vec, const Segments::iterator &sit, const unsigned k, const size_t N);
+                     Segments &segments, const ConvexFilter &convex_filter, const Segments::iterator &sit, const unsigned k, const size_t N);
 static bool isReflex(const cgal::Segment_2 &e_1, const cgal::Segment_2 &e_2, const cgal::Point_2 &shifted_point,
-                     Segments &segments, const std::vector<bool> &reflex_point_vec, const Segments::iterator &sit, const unsigned k, const size_t N, const size_t phase);
+                     Segments &segments, const ReflexFilter &reflex_filter, const Segments::iterator &sit, const unsigned k, const size_t N, const size_t phase);
 static bool isIntersection(const cgal::Segment_2 &e_1, const cgal::Segment_2 &e_2, Segments &segments, const Segments::iterator &sit);
 
 pl::PointList pl::bouncingVertices(const pl::PointList &point_list, const pl::CommonSettingList &common_settings, const pl::FilterList &filters) {
@@ -36,29 +104,14 @@ pl::PointList pl::bouncingVertices(const pl::PointList &point_list, const pl::Co
   Segments segments = init(point_list);
   auto [sampling_grid, phases, radius] = init(common_settings);
 
-  std::vector<bool> convex_point_vec(N, false);
-  std::vector<bool> reflex_point_vec(N, false);
-  std::optional<pl::Filter> convex_filter, reflex_filter;
+  ConvexFilter convex_filter = calculateConvexFilter(filters, N);
+  ReflexFilter reflex_filter = calculateReflexFilter(filters, N);
 
-  // if convex: choose random which point should stay
-  if (convex_filter = pl::find(filters, "convex points"))
-    for (int i = 0; i < (*convex_filter).val; ++i)
-      convex_point_vec[pl::randomValueOfRange(0, N)] = true;
+  if (convex_filter)
+    calculateConvexPoints(convex_filter, reflex_filter);
 
-  // if reflex: choose random which point should transform to reflex,
-  // choose not the same, as above!
-  // TODO:
-  // there are more conditions for the filter. because if there
-  // exists a convex point between two reflex points, the reflex point
-  // could not be fulfiled or vis versa.
-  if (reflex_filter = pl::find(filters, "reflex points"))
-    for (int i = 0; i < (*reflex_filter).val; ++i) {
-      int random_val = pl::randomValueOfRange(0, N);
-      while (convex_point_vec[random_val])
-        random_val = pl::randomValueOfRange(0, N);
-
-      reflex_point_vec[random_val] = true;
-    }
+  if (reflex_filter)
+    calculateReflexPoints(reflex_filter, segments, sampling_grid, radius, convex_filter);
 
   // if there is a chain, then above number list should be overriden
 
@@ -96,13 +149,13 @@ pl::PointList pl::bouncingVertices(const pl::PointList &point_list, const pl::Co
 
         // additional filters
         if (!intersection_occur) {
-          if (convex_filter && convex_point_vec[k])
-            filter_unsatisfied = !isConvex(e_1, e_2, shifted_point, segments, convex_point_vec, sit, k, N);
+          if (convex_filter && convex_filter.isSet(k))
+            filter_unsatisfied = !isConvex(e_1, e_2, shifted_point, segments, convex_filter, sit, k, N);
 
           // filter_unsatisfied does not have to be within this
           // condition, because those two values are mutual exclusive.
-          if (reflex_filter && reflex_point_vec[k])
-            filter_unsatisfied = !isReflex(e_1, e_2, shifted_point, segments, reflex_point_vec, sit, k, N, phase);
+          if (reflex_filter && reflex_filter.isSet(k))
+            filter_unsatisfied = !isReflex(e_1, e_2, shifted_point, segments, reflex_filter, sit, k, N, phase);
         }
       } while (intersection_occur || filter_unsatisfied);
 
@@ -183,6 +236,74 @@ Segments init(const pl::PointList &point_list) {
   return segments;
 }
 
+ConvexFilter calculateConvexFilter(const pl::FilterList &filters, size_t N) {
+  // -1 means no matter what number of convex points finally appear
+  int convex_points = -1;
+
+  if (auto convex_point_filter = pl::find(filters, "convex points"); convex_point_filter)
+    convex_points = (*convex_point_filter).val;
+
+  int convex_chains = -1;
+  if (auto convex_chain_filter = pl::find(filters, "convex chains"); convex_chain_filter)
+    convex_chains = (*convex_chain_filter).val;
+
+  return {N, convex_points, convex_chains};
+}
+
+ReflexFilter calculateReflexFilter(const pl::FilterList &filters, size_t N) {
+  int reflex_points = -1;
+  if (auto reflex_point_filter = pl::find(filters, "reflex points"); reflex_point_filter)
+    reflex_points = (*reflex_point_filter).val;
+
+  int reflex_chains = -1;
+  if (auto reflex_chain_filter = pl::find(filters, "reflex chains"); reflex_chain_filter)
+    reflex_chains = (*reflex_chain_filter).val;
+
+  return {N, reflex_points, reflex_chains};
+}
+
+// it is not necessary for the moment, that this function does use
+// reflex_filter, because this function get's called after calculateReflexPoints
+void calculateConvexPoints(ConvexFilter &convex_filter, const ReflexFilter &/*reflex_filter*/) {
+  for (int i = 0; i < convex_filter.point_max; ++i)
+    convex_filter.set(pl::randomValueOfRange(0, convex_filter.N));
+}
+
+// convex_filter is used to be sure, that the reflex points are not
+// set on the same index as the convex index. and that the reflex
+// points are far enough away.
+void calculateReflexPoints(ReflexFilter &reflex_filter, Segments &segments, const pl::SamplingGrid &sampling_grid, const double radius, const ConvexFilter &convex_filter) {
+  for (int i = 0; i < reflex_filter.point_max; ++i) {
+    int random_val;
+    do {
+      random_val = pl::randomValueOfRange(0, reflex_filter.N);
+    } while (convex_filter.isSet(random_val));
+
+    reflex_filter.set(random_val);
+  }
+
+  auto indices = reflex_filter.getIndices();
+
+  for (int reflex_index : indices) {
+    cgal::Segment_2 e_1, e_2;
+    cgal::Point_2 shifted_point;
+    auto sit = segments.begin() + reflex_index;
+    auto sitn = next(segments, sit); // not only add, boundary problems
+
+    do {
+      shifted_point = createPointInsideArea(sampling_grid, radius, sit);
+
+      e_1 = {sit->source(), shifted_point};
+      e_2 = {shifted_point, sitn->target()};
+    } while (isIntersection(e_1, e_2, segments, sit) || !isReflex(e_1, e_2, shifted_point, segments, reflex_filter, sit, reflex_index, reflex_filter.N, 0u));
+
+    *sit = e_1;
+    *sitn = e_2;
+  }
+}
+
+
+
 cgal::Point_2 createPointInsideArea(const pl::SamplingGrid &sampling_grid, const double radius, const Segments::iterator &sit) {
   cgal::Point_2 shifted_point;
 
@@ -199,19 +320,19 @@ cgal::Point_2 createPointInsideArea(const pl::SamplingGrid &sampling_grid, const
   return shifted_point;
 }
 
-bool isConvex(const cgal::Segment_2 &e_1, const cgal::Segment_2 &e_2, const cgal::Point_2 &shifted_point, Segments &segments, const std::vector<bool> &convex_point_vec, const Segments::iterator &sit, const unsigned k, const size_t N) {
+bool isConvex(const cgal::Segment_2 &e_1, const cgal::Segment_2 &e_2, const cgal::Point_2 &shifted_point, Segments &segments, const ConvexFilter &convex_filter, const Segments::iterator &sit, const unsigned k, const size_t N) {
   bool is_convex = false;
 
   auto sitn = next(segments, sit);
 
   bool next_angle_still_convex = true;
-  if (int l = (k+1 == N ? 0 : k+1); convex_point_vec[l]) {
+  if (int l = (k+1 == N ? 0 : k+1); convex_filter.isSet(l)) {
     auto sitnn = next(segments, sitn);
     next_angle_still_convex = CGAL::angle(shifted_point, sitn->target(), sitnn->target()) == CGAL::OBTUSE && CGAL::right_turn(shifted_point, sitn->target(), sitnn->target());
   }
 
   bool prev_angle_still_convex = true;
-  if (size_t l = (k == 0 ? N : k); next_angle_still_convex && convex_point_vec[l-1]) {
+  if (size_t l = (k == 0 ? N : k); next_angle_still_convex && convex_filter.isSet(l-1)) {
     auto sitp = prev(segments, sit);
     prev_angle_still_convex = CGAL::angle(sitp->source(), sitp->target(), shifted_point) == CGAL::OBTUSE && CGAL::right_turn(sitp->source(), sitp->target(), shifted_point);
   }
@@ -220,7 +341,7 @@ bool isConvex(const cgal::Segment_2 &e_1, const cgal::Segment_2 &e_2, const cgal
   // only has to be checked, if the prev(point) is still
   // convex if it has to be!
   if (prev_angle_still_convex && next_angle_still_convex)
-    is_convex = CGAL::angle(e_1.source(), shifted_point, e_2.target()) != CGAL::OBTUSE || !CGAL::right_turn(e_1.source(), shifted_point, e_2.target());
+    is_convex = CGAL::angle(e_1.source(), shifted_point, e_2.target()) == CGAL::OBTUSE && CGAL::right_turn(e_1.source(), shifted_point, e_2.target());
 
   // if the prev(point) has to be convex and it is not more,
   // but the current point does not have to be convex, the
@@ -233,7 +354,7 @@ bool isConvex(const cgal::Segment_2 &e_1, const cgal::Segment_2 &e_2, const cgal
   return is_convex;
 }
 
-bool isReflex(const cgal::Segment_2 &e_1, const cgal::Segment_2 &e_2, const cgal::Point_2 &shifted_point, Segments &segments, const std::vector<bool> &reflex_point_vec, const Segments::iterator &sit, const unsigned k, const size_t N, const size_t phase) {
+bool isReflex(const cgal::Segment_2 &e_1, const cgal::Segment_2 &e_2, const cgal::Point_2 &shifted_point, Segments &segments, const ReflexFilter &reflex_filter, const Segments::iterator &sit, const unsigned k, const size_t N, const size_t phase) {
   bool is_reflex = false;
 
   auto sitn = next(segments, sit);
@@ -242,19 +363,19 @@ bool isReflex(const cgal::Segment_2 &e_1, const cgal::Segment_2 &e_2, const cgal
   // could not be a reflex point. because the init algorithm
   // is the regular n-gon algorithm
   bool next_angle_still_reflex = true;
-  if (int l = (k+1 == N ? 0 : k+1); 0u < phase && reflex_point_vec[l]) {
+  if (int l = (k+1 == N ? 0 : k+1); 0u < phase && reflex_filter.isSet(l)) {
     auto sitnn = next(segments, sitn);
     next_angle_still_reflex = CGAL::angle(shifted_point, sitn->target(), sitnn->target()) == CGAL::ACUTE && CGAL::left_turn(shifted_point, sitn->target(), sitnn->target());
   }
 
   bool prev_angle_still_reflex = true;
-  if (size_t l = (k == 0 ? N : k); next_angle_still_reflex && reflex_point_vec[l-1]) {
+  if (size_t l = (k == 0 ? N : k); next_angle_still_reflex && reflex_filter.isSet(l-1)) {
     auto sitp = prev(segments, sit);
     prev_angle_still_reflex = CGAL::angle(sitp->source(), sitp->target(), shifted_point) == CGAL::ACUTE && CGAL::left_turn(sitp->source(), sitp->target(), shifted_point);
   }
 
   if (prev_angle_still_reflex && next_angle_still_reflex)
-    is_reflex = CGAL::angle(e_1.source(), shifted_point, e_2.target()) != CGAL::ACUTE  || !CGAL::left_turn(e_1.source(), shifted_point, e_2.target());
+    is_reflex = CGAL::angle(e_1.source(), shifted_point, e_2.target()) == CGAL::ACUTE && CGAL::left_turn(e_1.source(), shifted_point, e_2.target());
 
   // the same problem
   if ((!prev_angle_still_reflex || !next_angle_still_reflex) && !is_reflex)
