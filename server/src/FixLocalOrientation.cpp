a7c620dad55ed7f87f1f7b6b0bf3a1d45fe56f60
Christoph Ladurner <christoph.ladurner@tugraz.at>
2017-10-19 22:45:27 +0200

    FixLocalOrientation bugfix
    the distribution of the reflex nodes was not implemented correctly. it
    is easier to implement the distribution with the new init function.
    first the reflex points where distributed over the segments, and then
    the reflex points were calculated in a second loop.
    
    this change may also simplify further improvements for different
    reflex node distributions.

--- server/src/FixLocalOrientation.cpp
+++ server/src/FixLocalOrientation.cpp
@@ -15,6 +15,7 @@ using cgal = CGAL::Exact_predicates_inexact_constructions_kernel;
 
 static std::tuple<double> init(const pl::CommonSettingList &common_settings);
 static pl::CommonSettingList buildRegularPolygonSettings(const pl::CommonSettingList &common_settings, const pl::Filter &reflex_points);
+static std::vector<int> initReflexPointsPerSegment(const int node_counts, const int reflex_counts, const int reflex_chain_max);
 
 pl::PointList pl::fixLocalOrientation(pl::CommonSettingList &common_settings, const pl::FilterList &filters) {
   pl::PointList final_list;
@@ -51,6 +52,9 @@ pl::PointList pl::fixLocalOrientation(pl::CommonSettingList &common_settings, co
   // the segment angle
   double gamma = 2 * M_PI * 1/node_counts;
 
+  // the angle where new points could be added
+  double iota = (2*M_PI) - M_PI - gamma;
+
   // calculate distance from center of the regular polygon center to
   // point K. the point for the circle center on which resides the new
   // reflex points
@@ -70,99 +74,59 @@ pl::PointList pl::fixLocalOrientation(pl::CommonSettingList &common_settings, co
     throw std::runtime_error("center has no meaningfull value");
   }
 
-  // to do this, the given regular polygon has to have a vertical
-  // segment on between first and last point. this point is not
-  // necessary, because the vector d is rotated. but this comment is
-  // important because of the first sentence. this sentence says the
-  // precondition of this algorithm!
-  // cgal::Point_2 K(center.x + d.x(), center.y + d.y());
+  auto reflex_points_per_segment = initReflexPointsPerSegment(node_counts, reflex_counts, reflex_max);
 
-  std::map<int, bool> segments_used;
-  for (size_t i = 0; i < node_counts; ++i)
-    segments_used[i] = false;
+  for (int segment_number = node_counts - 1; 0 <= segment_number; --segment_number) {
+    // get number on reflex points to put in this segment
+    int reflex_counts_for_this_run = reflex_points_per_segment[segment_number];
 
-  size_t segments_count_used = 0;
+    if (0 < reflex_counts_for_this_run) {
+      // the rotation angle to rotate the vector from C to K = d
+      double omega = gamma * segment_number;
 
-  // while there are reflex points to init
-  while (0 < reflex_counts) {
-    // the random segment, where the reflex points lies in
-    int random_segment;
+      // this rotation has to be clockwise
+      CGAL::Aff_transformation_2<cgal> rotate_clockwise(CGAL::ROTATION, std::sin(-omega), std::cos(-omega));
 
-    // this construct might look ugly and bad for performance, but
-    // it's impackt is negligibly small. 
-    do {
-      random_segment = pl::randomValueOfRange(0, node_counts-1);
-    } while (segments_used[random_segment]);
+      // the random point L around which the reflex points are rotated,
+      // e is the vector from center to L
+      cgal::Vector_2 e = rotate_clockwise(d);
+      cgal::Point_2 L(center.x + e.x(), center.y + e.y());
 
-    segments_used[random_segment] = true;
-    segments_count_used += 1;
+      // the point P is the target of the segment where L is between
+      cgal::Point_2 P(point_list[segment_number].x, point_list[segment_number].y);
 
-    // the rotation angle to rotate the vector from C to K = d
-    double omega = gamma * random_segment;
+      // it is enough to calculate the it start point with
+      // final_list.begin() + segment_number, because the reflex
+      // segments where added from the end to the start. therefore
+      // from start to the current segment numbere all is fine!
+      auto insert_it = final_list.begin() + segment_number;
 
-    // this rotation has to be clockwise
-    CGAL::Aff_transformation_2<cgal> rotate_clockwise(CGAL::ROTATION, std::sin(-omega), std::cos(-omega));
+      // the vector l is the vector from L to the random point P
+      cgal::Vector_2 l = P - L;
 
-    // the random point L around which the reflex points are rotated,
-    // e is the vector from center to L
-    cgal::Vector_2 e = rotate_clockwise(d);
-    cgal::Point_2 L(center.x + e.x(), center.y + e.y());
+      // it has to be one more, because there are k points, but k+1
+      // segments to add, therefore k + 1 angles
+      double mu = iota / (reflex_counts_for_this_run + 1);
 
-    // the point P is the target of the segment where L is between
-    cgal::Point_2 P(point_list[random_segment].x, point_list[random_segment].y);
+      // while number of reflex points
+      while (0 < reflex_counts_for_this_run) {
+        // the angle could be calculated randomly. there is only the
+        // problem to calculate the angle randomly in such a way that
+        // reflex_counts_for_this_run + 1 angles could be calculated.
+        // the sum off all angles (mu) has to be less than iota
 
-    // this ensures that the current segment, which could be behind
-    // a just processed segment, would be found correctly. the
-    // calculation with "final_list.begin() + random_segment" is not
-    // enough because of the added reflex nodes of those segments
-    auto insert_it = std::find_if(final_list.begin(), final_list.end(), [&](auto p) { return p == P; });
+        // rotate clockwise around point L
+        CGAL::Aff_transformation_2<cgal> rotate_clockwise_l(CGAL::ROTATION, std::sin(-mu), std::cos(-mu));
+        l = rotate_clockwise_l(l);
 
-    // the vector l is the vector from L to the random point P
-    cgal::Vector_2 l = P - L;
+        // calculate next reflex point position
+        cgal::Point_2 P_r = L + l;
 
-    // if the reflex_chain_max is set, than is should be used to
-    // hinder that all reflex points where set into one chain. But it
-    // is not possible to protect against the possibility that the
-    // last segment takes all remaining reflex points. because if not
-    // then it would not possible to use all reflex points.
-    int max = 0 < reflex_max && reflex_max < reflex_counts ? reflex_max : reflex_counts;
+        // add point P_r to final_list between P and P+1
+        insert_it = final_list.insert(insert_it, {P_r.x(), P_r.y()});
 
-    // choose number on reflex points to put in this segment
-    int reflex_counts_for_this_run = pl::randomValueOfRange(1, max);
-
-    // overrule the random function, because it could be possible,
-    // that the random function does not put all possible reflex
-    // points automatically in the last segment.
-    if (segments_count_used == node_counts)
-      reflex_counts_for_this_run = reflex_counts;
-
-    reflex_counts -= reflex_counts_for_this_run;
-
-    // the angle where new points could be added
-    double iota = (2*M_PI) - M_PI - gamma;
-
-    // it has to be one more, because there are k points, but k+1
-    // segments to add, therefore k + 1 angles
-    double mu = iota / (reflex_counts_for_this_run + 1);
-
-    // while number of reflex points
-    while (0 < reflex_counts_for_this_run) {
-      // the angle could be calculated randomly. there is only the
-      // problem to calculate the angle randomly in such a way that
-      // reflex_counts_for_this_run + 1 angles could be calculated.
-      // the sum off all angles (mu) has to be less than iota
-
-      // rotate clockwise around point L
-      CGAL::Aff_transformation_2<cgal> rotate_clockwise_l(CGAL::ROTATION, std::sin(-mu), std::cos(-mu));
-      l = rotate_clockwise_l(l);
-
-      // calculate next reflex point position
-      cgal::Point_2 P_r = L + l;
-
-      // add point P_r to final_list between P and P+1
-      insert_it = final_list.insert(insert_it, {P_r.x(), P_r.y()});
-
-      reflex_counts_for_this_run -= 1;
+        reflex_counts_for_this_run -= 1;
+      }
     }
   }
 
@@ -209,4 +173,19 @@ pl::CommonSettingList buildRegularPolygonSettings(const pl::CommonSettingList &c
     regular_polygon_settings.push_back(*t);
 
   return regular_polygon_settings;
+}
+
+std::vector<int> initReflexPointsPerSegment(const int node_counts, const int reflex_counts, const int reflex_chain_max) {
+  std::vector<int> reflex_points_per_segment(node_counts, 0);
+
+  for (int i = 0; i < reflex_counts; ++i) {
+    unsigned pos;
+    do {
+      pos = pl::randomValueOfRange(0, node_counts-1);
+    } while (reflex_chain_max < reflex_points_per_segment[pos] + 1);
+
+    reflex_points_per_segment[pos] += 1;
+  }
+
+  return reflex_points_per_segment;
 }
