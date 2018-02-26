b1a10127248d5f4403f4b7fa878101b33b5660e5
Christoph Ladurner <christoph.ladurner@tugraz.at>
2018-02-26 20:34:18 +0100

    SpacePartitioning refactoring
    remove the std::optional return types. rearange functions that some of
    the static declared prototypes are not necessary

--- server/src/SpacePartitioning.cpp
+++ server/src/SpacePartitioning.cpp
@@ -1,4 +1,3 @@
-#include <optional>
 #include <algorithm>
 #include <limits>
 #include <cmath>
@@ -15,9 +14,6 @@
 using cgal = CGAL::Exact_predicates_inexact_constructions_kernel;
 
 static void recursiveDivide(pl::PointList &S, const cgal::Point_2 &s_f, const cgal::Point_2 &s_l, pl::PointList &C);
-static std::optional<cgal::Point_2> calculateRandomPoint(const pl::PointList &S, const cgal::Point_2 &s_f, const cgal::Point_2 &s_l);
-static std::optional<cgal::Line_2> calculateRandomLine(const cgal::Point_2 &s, const cgal::Point_2 &s_f, const cgal::Point_2 &s_l);
-static bool areOnSameSide(const cgal::Line_2 &l, const cgal::Point_2 &s_f, const cgal::Point_2 &s_l);
 
 pl::PointList pl::spacePartitioning(pl::PointList point_list) {
   pl::PointList final_list;
@@ -54,44 +50,9 @@ pl::PointList pl::spacePartitioning(pl::PointList point_list) {
   return final_list;
 }
 
-void recursiveDivide(pl::PointList &S, const cgal::Point_2 &s_f, const cgal::Point_2 &s_l, pl::PointList &C) {
-  if (S.size() == 2) {
-    C.push_back(s_l);
-  }
-  else if (S.size() == 3) {
-    cgal::Point_2 s;
-    for (auto p : S)
-      if (p != s_f && p != s_l)
-        s = p;
-
-    C.push_back(s);
-    C.push_back(s_l);
-  } else {
-    pl::random_selector<> selector{};
-
-    auto s = calculateRandomPoint(S, s_f, s_l);
-    auto l = calculateRandomLine(s.value(), s_f, s_l);
-
-    pl::PointList S_f = {s.value()}, S_l = {s.value()};
-
-    for (auto p : S)
-      // p < l
-      if (p != s.value() && l->has_on_positive_side(p))
-        S_f.push_back(p);
-      else if (p != s.value() && l->has_on_negative_side(p))
-        S_l.push_back(p);
-      else {
-        // TODO
-        // not handled points are collinear with the line l
-      }
-
-    recursiveDivide(S_f, s_f, s.value(), C);
-    recursiveDivide(S_l, s.value(), s_l, C);
-  }
-}
 
 
-std::optional<cgal::Point_2> calculateRandomPoint(const pl::PointList &S, const cgal::Point_2 &s_f, const cgal::Point_2 &s_l) {
+cgal::Point_2 calculateRandomPoint(const pl::PointList &S, const cgal::Point_2 &s_f, const cgal::Point_2 &s_l) {
   pl::random_selector<> selector{};
   cgal::Point_2 s;
 
@@ -108,11 +69,17 @@ std::optional<cgal::Point_2> calculateRandomPoint(const pl::PointList &S, const 
   return s;
 }
 
-std::optional<cgal::Line_2> calculateRandomLine(const cgal::Point_2 &s, const cgal::Point_2 &s_f, const cgal::Point_2 &s_l) {
+bool areOnSameSide(const cgal::Line_2 &l, const cgal::Point_2 &s_f, const cgal::Point_2 &s_l) {
+  // (s_f < l && s_l < l) || (l < s_f && l < s_l);
+  return (l.has_on_positive_side(s_f) && l.has_on_positive_side(s_l)) || (l.has_on_negative_side(s_f) && l.has_on_negative_side(s_l));
+}
+
+cgal::Line_2 calculateRandomLine(const cgal::Point_2 &s, const cgal::Point_2 &s_f, const cgal::Point_2 &s_l) {
   cgal::Line_2 l;
 
-  double delta_x = s_l.x() - s_f.x();
-  double delta_y = s_l.y() - s_f.y();
+  double
+    delta_x = s_l.x() - s_f.x(),
+    delta_y = s_l.y() - s_f.y();
 
   do {
     // implement heuristic....pdf implementation details about finding line
@@ -122,7 +89,8 @@ std::optional<cgal::Line_2> calculateRandomLine(const cgal::Point_2 &s, const cg
     if (std::abs(delta_x) < epsilon) {
       if (std::abs(delta_y) > epsilon)
         y = pl::randomValueOfRange(0.0, delta_y);
-    } else {
+    }
+    else {
       x = pl::randomValueOfRange(0.0, delta_x);
       y = (x * delta_y) / delta_x;
     }
@@ -139,8 +107,37 @@ std::optional<cgal::Line_2> calculateRandomLine(const cgal::Point_2 &s, const cg
   return l;
 }
 
-bool areOnSameSide(const cgal::Line_2 &l, const cgal::Point_2 &s_f, const cgal::Point_2 &s_l) {
-  // (s_f < l && s_l < l) || (l < s_f && l < s_l);
-  return (l.has_on_positive_side(s_f) && l.has_on_positive_side(s_l)) || (l.has_on_negative_side(s_f) && l.has_on_negative_side(s_l));
-}
+void recursiveDivide(pl::PointList &S, const cgal::Point_2 &s_f, const cgal::Point_2 &s_l, pl::PointList &C) {
+  if (S.size() == 2) {
+    C.push_back(s_l);
+  }
+  else if (S.size() == 3) {
+    cgal::Point_2 s;
+    for (auto p : S)
+      if (p != s_f && p != s_l)
+        s = p;
 
+    C.push_back(s);
+    C.push_back(s_l);
+  }
+  else {
+    cgal::Point_2 s = calculateRandomPoint(S, s_f, s_l);
+    cgal::Line_2 l = calculateRandomLine(s, s_f, s_l);
+
+    pl::PointList S_f = {s}, S_l = {s};
+
+    for (auto p : S)
+      // p < l
+      if (p != s && l.has_on_positive_side(p))
+        S_f.push_back(p);
+      else if (p != s && l.has_on_negative_side(p))
+        S_l.push_back(p);
+      else {
+        // TODO
+        // not handled points are collinear with the line l
+      }
+
+    recursiveDivide(S_f, s_f, s, C);
+    recursiveDivide(S_l, s, s_l, C);
+  }
+}
